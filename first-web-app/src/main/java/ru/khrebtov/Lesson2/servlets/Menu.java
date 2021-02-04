62609b985b649939ed049a1a04f6d07c44fbeac4
framzik <faradei33>
2021-02-04 10:56:09 +0300

    Lesson2

--- first-web-app/src/main/java/ru/khrebtov/Lesson2/servlets/Menu.java
+++ first-web-app/src/main/java/ru/khrebtov/Lesson2/servlets/Menu.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 @WebServlet(name = "menu")
 public class Menu extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<ul>" +
