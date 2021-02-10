84cbf6d67adf72638638e765c41bc2d3293e0e3a
Framzik <framzik@gmail.ru>
2021-02-10 21:19:43 +0300

    учел возможность переноса директорий(getServletContext().getContextPath())

--- first-web-app/src/main/java/ru/khrebtov/Lesson2/servlets/Menu.java
+++ first-web-app/src/main/java/ru/khrebtov/Lesson2/servlets/Menu.java
@@ -10,14 +10,15 @@ import java.io.IOException;
 @WebServlet(name = "menu")
 public class Menu extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<ul>" +
-                "<li><a href=/first-web-app/main>Главная.</a></li>" +
-                "<li><a href=/first-web-app/catalog>Каталог.</a></li>" +
-                "<li><a href=/first-web-app/product>Продукт.</a></li>" +
-                "<li><a href=/first-web-app/cart>Корзина.</a></li>" +
-                "<li><a href=/first-web-app/order>Заказ.</a>.</li>" +
-                "</ul>");
-    }
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.getWriter().println("<ul>" +
+        "<li><a href=" + getServletContext().getContextPath() + "/main>Главная.</a></li>" +
+        "<li><a href=" + getServletContext().getContextPath() + "/catalog>Каталог.</a></li>" +
+        "<li><a href=" + getServletContext().getContextPath() + "/product>Продукт.</a></li>" +
+        "<li><a href=" + getServletContext().getContextPath() + "/cart>Корзина.</a></li>" +
+        "<li><a href=" + getServletContext().getContextPath() + "/order>Заказ.</a>.</li>" +
+        "</ul>");
+  }
 }
